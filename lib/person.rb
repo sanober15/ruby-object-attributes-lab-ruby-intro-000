08c1accfaf382dd60d46452f36e67b689af7868f
sanober riaz <sanober@sanober.me>
2018-02-28 17:26:29 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,18 +1,18 @@
 class Person
   def name=(person_name)
-    @this_person_name = person_name
+    @name = person_name
   end
  
   def name
-    @this_person_name
+    @name
   end
   
   def job=(person_job)
-    @this_person_job =person_job
+    @job =person_job
   end
   
   def job
-    @this_person_job
+    @job
   end
 end
 
